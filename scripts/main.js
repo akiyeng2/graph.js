d3f77a0b5726dc561a89172c0dd80acdb7633ed8
scrblnrd3 <amalan.iyengar@gmail.com>
2014-07-06 17:32:59 -0400

    Added a lot of stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,43 @@
 var Graph = (function () {
-    function Graph(canvas, xMin, xMax, yMin, yMax) {
+    function Graph(canvas, xMin, xMax, yMin, yMax, lineWidth, pointWidth, axes, gridlines, tabs) {
         if (typeof xMin === "undefined") { xMin = -10; }
         if (typeof xMax === "undefined") { xMax = 10; }
         if (typeof yMin === "undefined") { yMin = -10; }
         if (typeof yMax === "undefined") { yMax = 10; }
+        if (typeof lineWidth === "undefined") { lineWidth = 1; }
+        if (typeof pointWidth === "undefined") { pointWidth = 1; }
+        if (typeof axes === "undefined") { axes = true; }
+        if (typeof gridlines === "undefined") { gridlines = true; }
+        if (typeof tabs === "undefined") { tabs = true; }
+        this._strokeStyle = "#000000";
+        this._fillStyle = "#000000";
+        this._maxTicks = 20;
         this.context = canvas.getContext("2d");
-        this.height = canvas.height;
-        this.width = canvas.width;
+        this._height = canvas.height;
+        this._width = canvas.width;
 
         this._xMin = xMin;
         this._xMax = xMax;
         this._yMin = yMin;
         this._yMax = yMax;
-        this._xLength = xMax - xMin;
-        this._yLength = yMax - yMin;
-        /*
-        How to find origin:
-        
-        x: [-10, 20]
-        (0, 300): (-10, 0)
-        (600, 300): (20, 0)
-        (300, 300): (5, 0)
-        (200, 300): (0, 0)
-        
-        600 / 30 = 20
-        
-        Distance from left to center: 10
-        Distance from center to right: 20
-        
-        0 + 10 * 20 = 200
-        600 - 20 * 20 = 200
-        
-        Conversion of origin to canvas: -xMin * width / xLength
-        Conversion of origin to canvas: -yMin * length / yLength
-        
-        y: [-10, 10]
-        length: 600
-        width: 600
-        origin(canvas): (150, 300)
-        
-        
-        
-        
-        */
+
+        this._lineWidth = lineWidth;
+        this._pointWidth = pointWidth;
+
+        this.context.lineWidth = lineWidth;
+
+        this._axes = axes;
+        this._gridLines = gridlines;
+        this._tabs = tabs;
+
+        this.update();
     }
     Object.defineProperty(Graph.prototype, "xMin", {
         get: function () {
             return this._xMin;
+        },
+        set: function (value) {
+            this._xMin = value;
         },
         enumerable: true,
         configurable: true
@@ -55,6 +46,31 @@ var Graph = (function () {
     Object.defineProperty(Graph.prototype, "xMax", {
         get: function () {
             return this._xMax;
+        },
+        set: function (value) {
+            this._xMax = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Graph.prototype, "yMin", {
+        get: function () {
+            return this._yMin;
+        },
+        set: function (value) {
+            this._yMin = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Graph.prototype, "yMax", {
+        get: function () {
+            return this._yMax;
+        },
+        set: function (value) {
+            this._yMax = value;
         },
         enumerable: true,
         configurable: true
@@ -68,22 +84,6 @@ var Graph = (function () {
         configurable: true
     });
 
-    Object.defineProperty(Graph.prototype, "yMin", {
-        get: function () {
-            return this._yMin;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Graph.prototype, "yMax", {
-        get: function () {
-            return this._yMax;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
     Object.defineProperty(Graph.prototype, "yLength", {
         get: function () {
             return this._yLength;
@@ -91,22 +91,169 @@ var Graph = (function () {
         enumerable: true,
         configurable: true
     });
+
+    Object.defineProperty(Graph.prototype, "width", {
+        get: function () {
+            return this._width;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Graph.prototype, "height", {
+        get: function () {
+            return this._height;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Graph.prototype.update = function () {
+        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        this._xLength = this._xMax - this._xMin;
+        this._yLength = this._yMax - this._yMin;
+
+        if (this._xLength <= 0) {
+            throw "xMax must be greater than or equal to xMin";
+        }
+
+        if (this._yLength <= 0) {
+            throw "yMax must be greater than or equal to yMin";
+        }
+
+        if (this._gridLines) {
+            var xScales = this.scale(this._xLength);
+            var yScales = this.scale(this._yLength);
+            this.drawGridlines(xScales[0], yScales[0]);
+            this.drawGridlines(xScales[1], yScales[1], "grey");
+
+            console.log(xScales, yScales);
+        }
+
+        if (this._axes) {
+            this.drawAxes();
+        }
+    };
+
+    Graph.prototype.drawCircle = function (center, radius) {
+        this.context.beginPath();
+        this.context.arc(center.x, center.y, radius, 0, Math.PI * 2, true);
+        this.context.fill();
+    };
+
+    Graph.prototype.point = function (x, y) {
+        return new Point(x, y, this);
+    };
+
+    Graph.prototype.drawPoint = function (point) {
+        this.drawCircle(point, this._pointWidth);
+    };
+
+    Graph.prototype.drawLine = function (point1, point2) {
+        this.context.beginPath();
+        this.context.moveTo(point1.x, point1.y);
+        this.context.lineTo(point2.x, point2.y);
+        this.context.stroke();
+    };
+
+    Graph.prototype.drawAxes = function () {
+        this.context.strokeStyle = "black";
+        this.context.lineWidth = 2;
+        this.drawLine(this.point(this._xMin, 0), this.point(this._xMax, 0));
+        this.drawLine(this.point(0, this._yMin), this.point(0, this._yMax));
+        this.context.lineWidth = this._lineWidth;
+    };
+
+    Graph.prototype.drawGridlines = function (xScale, yScale, color) {
+        if (typeof color === "undefined") { color = "lightgrey"; }
+        this.context.strokeStyle = color;
+
+        for (var i = 0; i < this._xMax; i += xScale) {
+            this.drawLine(this.point(i, this._yMin), this.point(i, this._yMax));
+        }
+
+        for (var i = 0; i > this._xMin; i -= xScale) {
+            this.drawLine(this.point(i, this._yMin), this.point(i, this._yMax));
+        }
+
+        for (var i = 0; i < this._yMax; i += yScale) {
+            this.drawLine(this.point(this._xMin, i), this.point(this._xMax, i));
+        }
+
+        for (var i = 0; i > this._yMin; i -= yScale) {
+            this.drawLine(this.point(this._xMin, i), this.point(this._xMax, i));
+        }
+    };
+
+    Graph.prototype.scale = function (length) {
+        var niceRange = this.makeNice(length, false)[0];
+        var scale = this.makeNice(niceRange / (this._maxTicks - 1), true);
+        return scale;
+    };
+
+    Graph.prototype.makeNice = function (num, round) {
+        var exponent = Math.floor(Math.log(num) / Math.log(10));
+        var fraction = num / Math.pow(10, exponent);
+        var niceFraction;
+
+        if (round) {
+            if (fraction < 1.5) {
+                niceFraction = 1;
+            } else if (fraction < 3) {
+                niceFraction = 2;
+            } else if (fraction < 7) {
+                niceFraction = 5;
+            } else {
+                niceFraction = 10;
+            }
+        } else {
+            if (fraction <= 1) {
+                niceFraction = 1;
+            } else if (fraction <= 2) {
+                niceFraction = 2;
+            } else if (fraction <= 5) {
+                niceFraction = 5;
+            } else {
+                niceFraction = 10;
+            }
+        }
+
+        var niceExponent = niceFraction * Math.pow(10, exponent);
+        if (niceFraction < 5) {
+            var bigExponent = niceExponent * 4;
+        } else {
+            var bigExponent = niceExponent * 5;
+        }
+
+        return [niceExponent, bigExponent];
+    };
     return Graph;
 })();
 
 var Point = (function () {
-    //	var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
+    //  var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
     function Point(x, y, graph) {
         var originX = -graph.xMin * graph.width / graph.xLength;
-        var originY = -graph.yMin * graph.height / graph.yLength;
+        var originY = graph.height + graph.yMin * graph.height / graph.yLength;
 
-        console.log(originX, originY);
+        this.x = originX + x * (graph.width / graph.xLength);
+        this.y = originY - y * (graph.height / graph.yLength);
     }
     return Point;
 })();
 
-var canvas = document.createElement("canvas");
-canvas.width = 500;
-canvas.height = 500;
-var graph = new Graph(canvas);
-new Point(0, 0, graph);
+var canvas = document.getElementById("graph");
+canvas.width = 600;
+canvas.height = 600;
+var graph = new Graph(canvas, 0, 100, 0, 10);
+
+function f(x) {
+    return Math.sqrt(x);
+}
+graph.context.strokeStyle = "black";
+for (var i = -10; i < 100; i += 100 / 600) {
+    var lastX = i - (100 / 600);
+    var lastY = f(lastX);
+
+    graph.drawLine(graph.point(lastX, lastY), graph.point(i, f(i)));
+}
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,6 @@
-class Graph {
-    
+
+class Graph {    
+
     context: CanvasRenderingContext2D;
     private _xMin: number;
     private _xMax: number;
@@ -7,109 +8,267 @@ class Graph {
     private _yMax: number;
     private _xLength: number;
     private _yLength: number;
-    private _origin: Point;
     
-    width: number;
-    height: number;
+    private _width: number;
+    private _height: number;
+
+    private _pointWidth: number;
+    private _lineWidth: number;
+
+    private _axes: boolean;
+    private _gridLines: boolean;
+    private _tabs: boolean;
     
-    
-    constructor(canvas: HTMLCanvasElement, xMin : number = -10, xMax : number = 10, yMin: number = -10, yMax: number = 10) {
+    private _strokeStyle: string = "#000000";
+    private _fillStyle: string = "#000000";
+
+    private _maxTicks: number = 20;
+    constructor(canvas: HTMLCanvasElement, xMin : number = -10, xMax : number = 10, yMin: number = -10, yMax: number = 10, 
+        lineWidth: number = 1, pointWidth: number = 1, axes: boolean = true, gridlines: boolean = true, tabs: boolean = true) {
+        
+
         this.context = canvas.getContext("2d");
-        this.height = canvas.height;
-        this.width = canvas.width;
+        this._height = canvas.height;
+        this._width = canvas.width;
         
         this._xMin = xMin;
         this._xMax = xMax;
         this._yMin = yMin;
         this._yMax = yMax;
-        this._xLength = xMax - xMin;
-        this._yLength = yMax - yMin;
-        
-        /*
-        How to find origin:
-        
-        x: [-10, 20]
-        (0, 300): (-10, 0)
-        (600, 300): (20, 0)
-        (300, 300): (5, 0)
-        (200, 300): (0, 0)
-        
-        600 / 30 = 20
-        
-        Distance from left to center: 10
-        Distance from center to right: 20
-        
-        0 + 10 * 20 = 200
-        600 - 20 * 20 = 200
-        
-        Conversion of origin to canvas: -xMin * width / xLength
-        Conversion of origin to canvas: -yMin * length / yLength
-        
-        y: [-10, 10]
-        length: 600
-        width: 600
-        origin(canvas): (150, 300)
-        
-        
-        
-        
-        */
        
-        
-        
-    }
+
+        this._lineWidth = lineWidth;
+        this._pointWidth = pointWidth;
+
+        this.context.lineWidth = lineWidth;
+
+        this._axes = axes;
+        this._gridLines = gridlines;
+        this._tabs = tabs;
+
+        this.update();
     
+    }
+     
     get xMin(): number {
         return this._xMin;
     }
-    
-     get xMax(): number {
+    set xMin(value: number) {
+        this._xMin = value;
+    }
+
+    get xMax(): number {
         return this._xMax;
     }
-    
-    get xLength(): number {
-        return this._xLength;
+    set xMax(value: number) {
+        this._xMax = value;
     }
-    
+
+
     get yMin(): number {
         return this._yMin;
     }
-    
+    set yMin(value: number) {
+        this._yMin = value;
+    }
+
     get yMax(): number {
         return this._yMax;
     }
+    set yMax(value: number) {
+        this._yMax = value;
+    }
     
+
+    get xLength(): number {
+        return this._xLength;
+    }
+
     get yLength(): number {
         return this._yLength;
     }
+
+    get width(): number {
+        return this._width;
+    }
+        
+    get height(): number {
+        return this._height;
+    }   
+
+    update() {
+        this.context.clearRect(0,0, this.context.canvas.width, this.context.canvas.height);
+        this._xLength = this._xMax - this._xMin;
+        this._yLength = this._yMax - this._yMin;
     
-    
-    
-    
-         
+        if(this._xLength <= 0) {
+            throw "xMax must be greater than or equal to xMin";
+        }
+
+        if(this._yLength <= 0) {
+            throw "yMax must be greater than or equal to yMin";
+        }
+
+        if(this._gridLines) {
+            var xScales: Array<number> = this.scale(this._xLength);
+            var yScales: Array<number> = this.scale(this._yLength);
+            this.drawGridlines(xScales[0], yScales[0]);
+            this.drawGridlines(xScales[1], yScales[1], "grey");
+
+            console.log(xScales, yScales);
+        }
+
+        if(this._axes) {
+            this.drawAxes();
+        }
+
+        
+
+
+    }
+
+       
+    drawCircle(center: Point, radius: number) {
+        this.context.beginPath();
+        this.context.arc(center.x, center.y, radius, 0, Math.PI * 2, true);
+        this.context.fill();
+    }
+
+    point(x: number, y: number): Point {
+        return new Point(x, y, this);
+    }
+
+    drawPoint(point: Point): void {
+        this.drawCircle(point, this._pointWidth);
+    }
+
+    drawLine(point1: Point, point2: Point): void {
+        this.context.beginPath();
+        this.context.moveTo(point1.x, point1.y);
+        this.context.lineTo(point2.x, point2.y);
+        this.context.stroke();
+
+    }
+
+    private drawAxes(): void {
+
+        this.context.strokeStyle = "black";
+        this.context.lineWidth = 2;
+        this.drawLine(this.point(this._xMin, 0), this.point(this._xMax, 0));
+        this.drawLine(this.point(0, this._yMin), this.point(0, this._yMax));
+        this.context.lineWidth = this._lineWidth;   
+
+    }
+
+    private drawGridlines(xScale: number, yScale: number, color: string = "lightgrey"): void {
+
+        this.context.strokeStyle = color;
+
+        for(var i = 0; i < this._xMax; i += xScale) {
+            this.drawLine(this.point(i, this._yMin), this.point(i, this._yMax));
+        }
+
+        for(var i = 0; i > this._xMin; i -= xScale) {
+            this.drawLine(this.point(i, this._yMin), this.point(i, this._yMax));
+        }
+
+
+
+        for(var i = 0; i < this._yMax; i += yScale) {
+            this.drawLine(this.point(this._xMin, i), this.point(this._xMax, i));
+        }
+
+        for(var i = 0; i > this._yMin; i -= yScale) {
+            this.drawLine(this.point(this._xMin, i), this.point(this._xMax, i));
+        }
+    }
+
+     
+    private scale(length: number): Array<number> {
+        var niceRange: number = this.makeNice(length, false)[0];
+        var scale: Array<number> = this.makeNice(niceRange / (this._maxTicks - 1), true);
+        return scale;
+
+    }
+
+    private makeNice(num: number, round: boolean): Array<number> {
+        var exponent: number = Math.floor(Math.log(num) / Math.log(10));
+        var fraction: number = num / Math.pow(10, exponent);
+        var niceFraction: number;
+
+        if(round) {
+            if(fraction < 1.5) {
+                niceFraction = 1;
+            } else if(fraction < 3) {
+                niceFraction = 2;
+            } else if(fraction < 7) {
+                niceFraction = 5;
+            } else {
+                niceFraction = 10;
+            }
+        } else {
+            if(fraction <= 1) {
+                niceFraction = 1;
+            } else if(fraction <= 2) {
+                niceFraction = 2;
+            } else if(fraction <= 5) {
+                niceFraction = 5;
+            } else {
+                niceFraction = 10;
+            }
+        }
+
+
+
+
+        var niceExponent = niceFraction * Math.pow(10, exponent);
+        if(niceFraction < 5) {
+            var bigExponent = niceExponent * 4;
+        } else {
+            var bigExponent = niceExponent * 5;
+        }
+
+        return [niceExponent, bigExponent];
+
+    }
+        
 }
 
 class Point {
-    private x: number;
-    private y: number;
-    
-    private canvasX: number;
-    private canvasY: number;
-    //	var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
+    x: number;
+    y: number;
+        
+    private gridX: number;
+    private gridY: number;
+    //  var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
 
     constructor(x: number, y: number, graph: Graph) {
         var originX = -graph.xMin * graph.width / graph.xLength;
-        var originY = -graph.yMin * graph.height / graph.yLength;
-        
-        console.log(originX, originY);
-        
+        var originY = graph.height + graph.yMin * graph.height / graph.yLength;
+
+
+
+        this.x = originX + x * (graph.width / graph.xLength);
+        this.y = originY - y * (graph.height / graph.yLength);
+
+           
     }
     
     
 }
 
-var canvas = document.createElement("canvas");
-canvas.width = 500;
-canvas.height = 500;
-var graph = new Graph(canvas);
-new Point(0, 0, graph);
+var canvas: any = document.getElementById("graph");
+canvas.width = 600;
+canvas.height = 600;
+var graph = new Graph(canvas, 0, 100, 0, 10);
+
+function f(x: number): number {
+    return Math.sqrt(x);
+}
+graph.context.strokeStyle = "black";
+for(var i = -10; i < 100; i+= 100 / 600) {  
+    var lastX = i - (100  / 600);
+    var lastY = f(lastX);
+
+    graph.drawLine(graph.point(lastX, lastY), graph.point(i, f(i)));
+}
