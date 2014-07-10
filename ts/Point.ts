1c5cbf33af243a74dceb5a082d308263c253cd68
scrblnrd3 <amalan.iyengar@gmail.com>
2014-07-10 15:35:49 -0400

    New branch for interface Drawable

--- ts/Point.ts
+++ ts/Point.ts
@@ -1,23 +1,52 @@
-/*global $:false */
-class Point {
+// /*global $:false */
+// class Point {
+//     x: number;
+//     y: number;
+
+//     private gridX: number;
+//     private gridY: number;
+//     //  var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
+
+//     constructor(x: number, y: number, graph: Graph) {
+//         var originX = -graph.xMin * graph.width / graph.xLength;
+//         var originY = graph.height + graph.yMin * graph.height / graph.yLength;
+
+
+
+//         this.x = originX + x * (graph.width / graph.xLength);
+//         this.y = originY - y * (graph.height / graph.yLength);
+
+
+//     }
+
+
+// }
+
+
+class Point implements Drawable {
     x: number;
     y: number;
 
-    private gridX: number;
-    private gridY: number;
-    //  var newX=x*(this.settings.width/this.settings.xlength),newY=-y*(this.settings.height/this.settings.ylength);
+    private _graphX: number;
+    private _graphY: number;
 
     constructor(x: number, y: number, graph: Graph) {
         var originX = -graph.xMin * graph.width / graph.xLength;
         var originY = graph.height + graph.yMin * graph.height / graph.yLength;
 
-
-
         this.x = originX + x * (graph.width / graph.xLength);
         this.y = originY - y * (graph.height / graph.yLength);
 
+        this._graphX = x;
+        this._graphY = y;
 
     }
 
+    public get graphX(): number {
+        return this._graphX;
+    }
 
+    public get graphy(): number {
+        return this._graphY;
+    }
 }
