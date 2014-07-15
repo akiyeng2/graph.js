798c7a49a1b7bb75f92e24a50bada7c007e5d3d1
scrblnrd3 <amalan.iyengar@gmail.com>
2014-07-15 14:16:26 -0400

    Added uglified version

--- Equation/Simplify.js
+++
@@ -1,9 +0,0 @@
-Equation.prototype.standardize = function() {
-	this.tree = this.tree.standardize();
-	return this;
-}
-
-Equation.prototype.simplify = function() {g
-	this.tree = this.tree.simplify();	
-}
-
